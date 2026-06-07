233694ef04c79665f263b3d61b15f6a2eb1b5844
A. Baco <alicee.baco@gmail.com>
2026-06-07 22:30:36 +0200

    feat : implement A* algorithm

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -98,9 +98,135 @@ public class Algorithms {
         return return_value;
     }
 
-    public static HashMap<String,HashMap<?,?>> AEtoile(Graphe g,String depart,String arrivee){
-        return null;
+    /**
+     * 
+     * Calcule la valeur de h pour un graphe TSPLIB donné
+     * @param g graphe choisi
+     * @param x sommet
+     * @param destination 2eme sommet, dans le cadre de l'algo suivant, la destination
+     * @return valeur 
+     */
+    public static double h(GrapheTSPLIB g,String x, String destination) throws Exception{
+        Double res = g.getDistanceSommets(x, destination);
+        if (res == Double.MAX_VALUE){
+            throw new Exception("Tentative de calcul de distance sur des sommets sans coordonnées ! Sommets concernés : " + x + ", "+destination+".");
+        }
+        return res;
+    }
+    /**
+     * Implementation java de l'algorithme A*
+     * La fonction nommée h(x) sera implémentée de maniere fixe dans ce fichier, dans le cadre d'une application plus complete, la signature de la fonction serait modifiée pour passer la fonction h en parametre
+     * 
+     * mettre pi de tout sommet a inf
+     * mettre T de tout sommet a null
+     * nouvelle list  par rapport a dijkstra -> f pour chaque sommet on va calculer sa valeur dans la fonction d'evalution, ici la distance euclienne par rapport a la destination on doit trouver le min dans cette liste donc on la modélisera par un tas qui remplacera le S de dijkstra
+     * 
+     * pi depart a 0
+     * on bind un string s a r
+     * tant que s ne vaut pas arrivee faire
+     *      marquer s
+     *      pour tous les voisins de s
+     *          idem dijkstra
+     *          f[voisin_actuel] = pi[voisin_actuel] + h(voisin_actuel)
+     *      s = minimum de f -> f doit être un tas
+     * 
+     * @param g grapheTSPLIB contenant pour chaque sommet des coordonnées correspondantes
+     * @param depart sommet de depart
+     * @param arrivee sommet d'arrivée
+     * @return une hashmap contenant deux hashmap : pi et T
+     */
+    public static HashMap<String,HashMap<?,?>> AEtoile(GrapheTSPLIB g,String depart,String arrivee) throws Exception{
+
+        //liste des sommets marqués
+        //hashset pour avoir une complexité meilleure qu'avec une arraylist
+        HashSet<String> sommets_traites = new HashSet<>();
+        ArrayList<String> sommets_graphe = g.getSommets();
+
+        //si le sommet de depart ou le sommet d'arrivée n'est pas dans le graphe, on sort du programme
+        if(!sommets_graphe.contains(depart) || !sommets_graphe.contains(arrivee)){
+            return null;
+        }
+
+        HashMap<String,Integer> pi = new HashMap<>();
+        HashMap<String,String> T = new HashMap<>();
+        Tas f = new Tas();
+
+        for (String sommet : sommets_graphe) {
+            //on utilise maxvalue pour representer l'infini et null pour un -1 car ici on fonctionnera toujours en string
+            pi.put(sommet,Integer.MAX_VALUE);
+            T.put(sommet,null); 
+            
+            //le tas utilise des entiers ici le resultat sera un double, on supposera que en cas d'arrondi égal le calcul n'aurait pas été impacté
+            //on peut envisager une implantation de tas paramétrable pour le type de donnée passé
+            int h_value;
+            try{
+                h_value = (int) Math.floor(h(g,sommet,arrivee));
+            }catch(Exception e){
+                throw new Exception("Erreur depuis l'algorithme A* :" + e.getMessage());
+            }
+            f.ajouter(sommet, h_value);
+        }
+
+        pi.replace(depart, 0);
+
+        String s = depart; //sommet
+
+        while (s!=arrivee){
+
+            sommets_traites.add(s);
+            //si le sommet est marqué on va au suivant
+
+
+            ArrayList<String> voisins = g.getVoisins(s);
+            for(String voisin_actuel : voisins){
+                //si le voisin est marqué on va au suivant
+                if(sommets_traites.contains(voisin_actuel)){
+                    continue;
+                }
+                if (pi.get(s) == Integer.MAX_VALUE) break; //si le sommet le plus proche est +inf donc pas de sommet voisin on ne traite pas les voisins de ce sommet et on continue l'algo 
+                
+                Integer distance_s_v = g.getDistance(s, voisin_actuel); //integer au lieu de int pour pouvoir faire la comparaison a null
+
+                if(distance_s_v == null ){
+                    System.err.println("[Algorithms.java][Dijkstra] Probleme lors de la verification de distance avec les voisins");
+                    continue; //si il y a un probleme on boucle sur le sommet suivant
+                }
+
+                
+                int nouvelle_distance = pi.get(s) + distance_s_v;
+
+                if (nouvelle_distance < pi.get(voisin_actuel)) 
+                {
+                    pi.put(voisin_actuel, nouvelle_distance);
+                    T.put(voisin_actuel, s);
+                    int h_value;
+                    try{
+                        h_value = (int) Math.floor(h(g,voisin_actuel,arrivee));
+                    }catch(Exception e){
+                        throw new Exception("Erreur depuis l'algorithme A* :" + e.getMessage());
+                    }
+                    f.modifierPriorite(voisin_actuel, nouvelle_distance+h_value); //f(u) = pi(u) + h(u)
+                }            
+        }   
+            if(f.estVide()) break; //plus de nouveau sommet a explorer, on sort de l'algorithme
+            Map.Entry<String, Integer> result = f.retirer();
+            String res = result.getKey(); // on prend s non marqué tq f(s) minimum
+            while(sommets_traites.contains(res)){
+                if(f.estVide()) break; //plus de nouveau sommet a explorer, on sort de l'algorithme
+                // si le sommet récuperé est marqué, on en récupere un autre
+                result = f.retirer();
+                res = result.getKey(); // on prend s non marqué tq f(s) minimum
+            }
+            s = res;
+
+      
+        }
+
+        HashMap<String, HashMap<?,?>> return_value = new HashMap<>();
+        return_value.put("pi",pi);
+        return_value.put("T",T);
+        return return_value;
     }
 
-    
+
 }
